7880b0ff58eb01ed66eb79d666e39cbc0a31560f
Станислав Андреевич <gr411_rastan@lab-407-3.tomtit-tomsk.ru>
2023-03-06 14:36:40 +0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inventory: 1
+    }
+})
+
+
